3aa33fd47bce22b17651ec9d91502fb6cc1095d5
Kei Fujikawa <fujikei22funk@gmail.com>
2018-09-19 00:22:11 +0900

    tongue detection sample

--- ARKit-Emperor/View/Face/FaceViewController.swift
+++ ARKit-Emperor/View/Face/FaceViewController.swift
@@ -10,30 +10,41 @@ import UIKit
 import ARKit
 
 class FaceViewController: UIViewController {
-    @IBOutlet var sceneView: ARSCNView!
+    @IBOutlet weak var arSceneView: ARSCNView!
+    @IBOutlet weak var previewSceneView: SCNView!
     
     let defaultConfiguration: ARFaceTrackingConfiguration = {
         let configuration = ARFaceTrackingConfiguration()
         return configuration
     }()
     
+    private lazy var airPlaneNode: SCNNode = previewSceneView.scene!.rootNode.childNode(withName: "ship", recursively: true)!
+    
+    var isAnimating = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        sceneView.delegate = self
-        sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
+        arSceneView.delegate = self
+        arSceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
+        
+        previewSceneView.scene = SCNScene(named: "art.scnassets/ship.scn")
+        previewSceneView.alpha = 0.8
+        previewSceneView.layer.cornerRadius = 5
+        previewSceneView.clipsToBounds = true
+        previewSceneView.isPlaying = true
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        sceneView.session.run(defaultConfiguration)
+        arSceneView.session.run(defaultConfiguration)
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         
-        sceneView.session.pause()
+        arSceneView.session.pause()
     }
     
 }
@@ -44,14 +55,25 @@ extension FaceViewController: ARSCNViewDelegate {
         guard let faceAnchor = anchor as? ARFaceAnchor else {
             return
         }
-        print(faceAnchor.rightEyeTransform)
     }
     
     func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
         guard let faceAnchor = anchor as? ARFaceAnchor else {
             return
         }
-        print(faceAnchor.rightEyeTransform)
+        
+        if let tongueOut = faceAnchor.blendShapes[.tongueOut] as? Float {
+            
+            if tongueOut > 0.5, !isAnimating {
+                isAnimating = true
+                
+                let action = SCNAction.rotateBy(x: 0, y: 0, z: .pi, duration: 0.3)
+                
+                airPlaneNode.runAction(action, completionHandler: {
+                    self.isAnimating = false
+                })
+            }
+        }
     }
 }
 
