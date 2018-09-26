8c4cc66ba989165a3f0e2993840ce3a46fe52622
Kei Fujikawa <fujikei22funk@gmail.com>
2018-09-26 19:18:58 +0900

    refactoring.

--- ARKit-Emperor/View/Remote/RemoteViewController.swift
+++ ARKit-Emperor/View/Remote/RemoteViewController.swift
@@ -18,7 +18,23 @@ class RemoteViewController: UIViewController {
         return configuration
     }()
     
+    lazy var objectSaveURL: URL = {
+        do {
+            return try FileManager.default
+                .url(for: .documentDirectory,
+                     in: .userDomainMask,
+                     appropriateFor: nil,
+                     create: true)
+                .appendingPathComponent("remote.arobject")
+        } catch {
+            fatalError("Can't get file save URL: \(error.localizedDescription)")
+        }
+    }()
+    
     private var boundingBoxNode: SCNNode?
+    private var detectedNodes: [SCNNode] = []
+    
+    private let boxExtent: float3 = float3(0.1, 0.05, 0.2)
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -43,30 +59,28 @@ class RemoteViewController: UIViewController {
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         if let fixedBoxNode = boundingBoxNode?.clone() {
             fixedBoxNode.opacity = 0.8
+            
+            // TODO: show keyboard to add name ?
+            fixedBoxNode.name = "detectedRemote"
+            
+            detectedNodes.append(fixedBoxNode)
+            
             self.sceneView.scene.rootNode.addChildNode(fixedBoxNode)
         }
     }
     
-    private func createReferenceObject(completionHandler creationFinished: @escaping (ARReferenceObject?) -> Void) {
-        guard let boundingBox = boundingBoxNode else {
-            creationFinished(nil)
-            return
+    private func showOKDialog(title: String, message: String? = nil, ok: String = "OK", completion: (() -> Void)? = nil){
+        DispatchQueue.main.async {
+            let alert: UIAlertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+            let defaultAction = UIAlertAction(title: ok, style: .default, handler: { _ in
+                completion?()
+            })
+            alert.addAction(defaultAction)
+            self.present(alert, animated: true, completion: nil)
         }
-        sceneView.session.createReferenceObject(
-            transform: boundingBox.simdTransform,
-            center: float3(),
-            extent: float3(),
-            completionHandler: { object, error in
-                
-                if let error = error {
-                    print(error.localizedDescription)
-                    creationFinished(nil)
-                }else if let object = object {
-                    creationFinished(object)
-                }
-        })
     }
     
+    // FIXME:
     private func setReferenceObject(_ object: ARReferenceObject) {
         let configuration = ARWorldTrackingConfiguration()
         configuration.detectionObjects = [object]
@@ -82,7 +96,7 @@ class RemoteViewController: UIViewController {
             var position = SCNVector3(float3)
             
             // half of box's height
-            position.z += 0.025
+            position.z += boxExtent.y / 2
             boundingBox.position = position
             
             if let camera = sceneView.pointOfView {
@@ -103,7 +117,10 @@ class RemoteViewController: UIViewController {
             return nil
         }
         let boundingBoxNode = SCNNode()
-        let box = SCNBox(width: 0.1, height: 0.05, length: 0.2, chamferRadius: 0)
+        let box = SCNBox(width: CGFloat(boxExtent.x),
+                         height: CGFloat(boxExtent.y),
+                         length: CGFloat(boxExtent.z),
+                         chamferRadius: 0)
         box.firstMaterial?.diffuse.contents = UIColor.yellow
         boundingBoxNode.geometry = box
         boundingBoxNode.opacity = 0.25
@@ -119,11 +136,28 @@ class RemoteViewController: UIViewController {
     }
 
     @IBAction func scanButtonTapped(_ sender: Any) {
-        createReferenceObject(completionHandler: { object in
-            if let object = object {
-                self.setReferenceObject(object)
-            }
-        })
+        if let node = detectedNodes.first {
+            sceneView.session.createReferenceObject(
+                transform: node.simdTransform,
+                center: float3(node.position),
+                extent: boxExtent,
+                completionHandler: { object, error in
+                    
+                    if let error = error {
+                        DispatchQueue.main.async {
+                            self.showOKDialog(title: error.localizedDescription)
+                        }
+                    }else if let object = object {
+                        do {
+                            try object.export(to: self.objectSaveURL, previewImage: nil)
+                        } catch {
+                            print(error.localizedDescription)
+                            self.showOKDialog(title: error.localizedDescription)
+                        }
+                        
+                    }
+            })
+        }
     }
 }
 
@@ -138,7 +172,7 @@ extension RemoteViewController: ARSCNViewDelegate {
     
     func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
         if let objectAnchor = anchor as? ARObjectAnchor {
-            print("detected!")
+            print("detected!: \(anchor.name)")
         }
     }
 }
