1815415746071d6be058d5e697372f13e9f5a0b2
Kei Fujikawa <fujikei22funk@gmail.com>
2018-12-02 01:14:24 +0900

    create star polygon

--- ARKit-Emperor/View/CustomModel/CustomModelViewController.swift
+++ ARKit-Emperor/View/CustomModel/CustomModelViewController.swift
@@ -41,22 +41,19 @@ class CustomModelViewController: UIViewController {
     }
     
     private func createStar(){
-        let path = UIBezierPath()
-        path.move(to: CGPoint(x: 0.1, y: 0.5))       //A
-        path.addLine(to: CGPoint(x: 0.1, y: 0.1))    //B
-        path.addLine(to: CGPoint(x: 0.3, y: 0.1))       //C
-        path.addLine(to: CGPoint(x: -0.1, y: -0.5))  //D
-        path.addLine(to: CGPoint(x: -0.1, y: -0.1))  //E
-        path.addLine(to: CGPoint(x: -0.3, y: -0.1))  //F
-        path.close()
-        
-        let shape = SCNShape(path: path, extrusionDepth: 0.2)
+        let starPath = UIBezierPath
+            .polygon(.starVertexes(
+                in: CGRect(origin: .zero, size: CGSize(width: 1, height: 1)),
+                roundness: 50,
+                numberOfVertexes: 5))
+        let shape = SCNShape(path: starPath, extrusionDepth: 0.2)
         let color = #colorLiteral(red: 1, green: 0.9913478494, blue: 0, alpha: 1)
         shape.firstMaterial?.diffuse.contents = color
-        shape.chamferRadius = 0.1
+        shape.chamferRadius = 0.08
         
         let boltNode = SCNNode(geometry: shape)
         boltNode.position.z = -1
+        boltNode.eulerAngles.x = .pi
         sceneView.scene.rootNode.addChildNode(boltNode)
     }
 }
@@ -65,5 +62,36 @@ extension CustomModelViewController: ARSCNViewDelegate {
     
     func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
         
+    }
+}
+
+// MARK: - Array<CGPoint>
+private extension Array where Element == CGPoint {
+    static func starVertexes(in frame: CGRect, roundness: CGFloat, numberOfVertexes vertexes: Int) -> [CGPoint] {
+        return starVertexes(
+            radius: Swift.min(frame.width, frame.height)/2,
+            center: CGPoint(x: frame.midX, y: frame.midY),
+            roundness: Swift.max(Swift.min(roundness, 100), 0)/100,
+            numberOfVertexes: Swift.max(vertexes, 2))
+    }
+    
+    private static func starVertexes(radius: CGFloat, center: CGPoint, roundness: CGFloat, numberOfVertexes vertexes: Int) -> [CGPoint] {
+        let vertexes = (vertexes * 2)
+        return [Int](0...vertexes).map { offset in
+            let r = (offset % 2 == 0) ? radius : roundness * radius
+            let θ = CGFloat(offset)/CGFloat(vertexes) * (2 * CGFloat.pi) - CGFloat.pi/2
+            return CGPoint(x: r * cos(θ) + center.x, y: r * sin(θ) + center.y)
+        }
+    }
+}
+
+// MARK: - UIBezierPath
+private extension UIBezierPath {
+    static func polygon(_ vertexes: [CGPoint]) -> UIBezierPath {
+        let path = UIBezierPath()
+        path.move(to: vertexes.first!)
+        vertexes.forEach { path.addLine(to: $0) }
+        path.close()
+        return path
     }
 }
