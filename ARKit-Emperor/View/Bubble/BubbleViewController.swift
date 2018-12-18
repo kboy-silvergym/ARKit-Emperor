9eec33f573333671e2a46deac6e945e3758e09b3
Kei Fujikawa <fujikei22funk@gmail.com>
2018-12-18 09:30:52 +0900

    add default light

--- ARKit-Emperor/View/Bubble/BubbleViewController.swift
+++ ARKit-Emperor/View/Bubble/BubbleViewController.swift
@@ -22,6 +22,7 @@ class BubbleViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        sceneView.autoenablesDefaultLighting = true
         sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
     }
     
